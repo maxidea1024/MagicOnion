66ce31355f66399baf5dec2ae126e16bde1d3700
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-11-06 19:59:02 +0900

    feat: use AgonesPod nuget package

--- samples/ChatApp/ChatApp.Match/AgonesPod/Internal/Utf8Json/Internal/StringEncoding.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-
-namespace AgonesPod.Internal.Utf8Json.Internal
-{
-    internal static class StringEncoding
-    {
-        public static readonly Encoding UTF8 = new UTF8Encoding(false);
-    }
-}
